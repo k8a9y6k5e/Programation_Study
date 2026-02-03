186d34119a4d56d5a45d31b4adaba6bf7e5e5a12
k9a8y6k5e <kaykekk8965@gmail.com>
2026-02-03 16:21:22 -0300

    new project starting, project about a auth app

---
+++ practice/APIs/Auth-API/app.js
@@ -0,0 +1,18 @@
+/*
+ROADMAP:
+*Log In
+**way to enter with
+***password
+***email
+**validator in each value enter
+**show informations
+*use jwt to auth
+*use bcrypt in password
+*Sign Up
+**acount creation with:
+***pasword
+***email
+***username
+**add a validator in each value
+**add an id in user
+*/
