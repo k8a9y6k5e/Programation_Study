2975fc62cfacec59b5068292d87e4f5263bff434
k9a8y6k5e <kaykekk8965@gmail.com>
2026-02-03 16:56:30 -0300

    sign up controller to create the account added

--- practice/APIs/Auth-API/app.js
+++ practice/APIs/Auth-API/app.js
@@ -1,3 +1,8 @@
+
+
+//to save the secret, because is a study program
+process.env.JWT_secret = "70b7c0a466a9e99e83e3c268a385cf39f7b0b724ac87c52f721d19b911df9950";
+
 /*
 ROADMAP:
 *Log In
@@ -9,7 +14,7 @@ ROADMAP:
 *use jwt to auth
 *use bcrypt in password
 *Sign Up
-**acount creation with:
+**account creation with:
 ***pasword
 ***email
 ***username
---
+++ practice/APIs/Auth-API/controller/sign-up-controller.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const {randomUUID} = require('crypto');
+
+function creationAccountController(req,res,next){
+    try{
+        const id = randomUUID();
+
+        const token = _tokenCreation(req.validatedBody, id)
+
+        res.status(201).json({work:true, data:token});
+    }
+    catch(err){
+        next(err);
+    }
+}
+
+function _tokenCreation(userInformations, id) {
+    const payload = {
+        username : userInformations.username,
+        email : userInformations.email,
+        id : id
+    }
+
+    const token = jwt.sign(payload, process.env.JWT_secret,{expiresIn : '7d'});
+    
+    return token;
+}
